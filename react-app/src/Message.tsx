46ce241c3dfd82dadcf2ec92558928ec18bd0078
Pooof Jikly <jiklyp000f@yandex.ru>
2025-06-04 15:29:56 +0300

    исправил следовательно заданию, а конкретнее передал значение Message через пропс

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,3 +1,3 @@
-export default function Message(){
-    return <h1 className="text-3xl font-bold underline">Message</h1>;
+export default function Message(props: { text: string }) {
+    return <h1 className="text-3xl font-bold underline">{props.text}</h1>;
 }
