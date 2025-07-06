19c6c6f2f1c15786f7dfa61d640c313da6802d9b
Pooof Jikly <jiklyp000f@yandex.ru>
2025-07-06 13:49:40 +0300

    исправил баг с прелоадером(до исправления не был добавлен в лейаут)

--- react-app/src/Layout.tsx
+++ react-app/src/Layout.tsx
@@ -4,6 +4,7 @@ import type { ModalHandlers } from './Navbar/Modal';
 import { useRef } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Footer } from './Footer/Footer';
+import Preloader from './Preloader/Preloader';
 
 function Layout() {
 	const modalRef = useRef<ModalHandlers>(null);
@@ -16,6 +17,7 @@ function Layout() {
 
 	return (
 		<>
+			<Preloader />
 			<Modal ref={modalRef} />
 			<Navbar onLoginClick={handleLoginClick} />
 			<Outlet />
